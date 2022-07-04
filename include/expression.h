e1d783aaca980dc30afaffffad40a98f46f5c194
Artem Koton <kotonartem@protonmail.com>
2022-07-04 17:22:24 +0300

    Use unique_ptr for clone (and update tests)

--- include/expression.h
+++ include/expression.h
@@ -3,14 +3,15 @@
 #include "int128.h"
 
 #include <map>
+#include <memory>
 #include <ostream>
 #include <string>
 
 struct Expression
 {
-    virtual Int128 eval(std::map<std::string, Int128> const & values = {}) const = 0;
+    virtual Int128 eval(const std::map<std::string, Int128> & values = {}) const = 0;
 
-    virtual Expression * clone() const = 0;
+    virtual std::unique_ptr<Expression> clone() const = 0;
 
     friend std::ostream & operator<<(std::ostream & out, const Expression & expression);
 };
